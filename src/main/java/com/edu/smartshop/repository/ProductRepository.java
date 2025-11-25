bacdd5ad1c14ff49bfdb70b8acf33d1a45657313
Oujaber Ousama <oujaberousama@gmail.com>
2025-11-25 14:48:18 +0100

    feat: add ProductRepository for product data access

---
+++ src/main/java/com/edu/smartshop/repository/ProductRepository.java
@@ -0,0 +1,13 @@
+package com.edu.smartshop.repository;
+
+import com.edu.smartshop.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    Optional<Product> findByName(String name);
+    boolean existsByName(String name);
+}
