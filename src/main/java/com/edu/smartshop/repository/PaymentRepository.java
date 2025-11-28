3cb8332a5158ffbab52ca8b0ea52c42da3b85f48
Oujaber Ousama <oujaberousama@gmail.com>
2025-11-28 09:50:18 +0100

    feat: add Repository

---
+++ src/main/java/com/edu/smartshop/repository/PaymentRepository.java
@@ -0,0 +1,12 @@
+package com.edu.smartshop.repository;
+
+import com.edu.smartshop.entity.Payment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface PaymentRepository extends JpaRepository<Payment, Long> {
+    List<Payment> findByOrderId(Long orderId);
+}
---
+++ src/main/java/com/edu/smartshop/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.edu.smartshop.repository;
+
+import com.edu.smartshop.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+    boolean existsByUsername(String username);
+}
